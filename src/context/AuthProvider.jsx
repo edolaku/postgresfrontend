e7fc54b5c965544590c7cd2d8673322833c2f363
edolaku <edoooq@gmail.com>
2024-10-07 15:49:38 +0700

    akan melanjutkan setPersist ke localStorage

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -5,7 +5,7 @@ const AuthContext = createContext();
 // eslint-disable-next-line react/prop-types
 export const AuthProvider = ({ children }) => {
     const [auth, setAuth] = useState({});
-    const [persist, setPersist] = useState(JSON.parse(localStorage.getItem("persist") || "false"));
+    // const [persist, setPersist] = useState(JSON.parse(localStorage.getItem("persist") || "false"));
 
     // console.log("auth dari AuthProvider: ", auth);
     return (
