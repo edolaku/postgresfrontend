0603bff862359a3ed709c0e0f071b83af3e41486
edolaku <edoooq@gmail.com>
2024-10-26 21:27:49 +0700

    SELESAI auth login menggunakn RTK (tambah authorization admin & user)

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from "./components/Login"
 import Welcome from "./components/Welcome"
 import RequireAuth from "./components/RequireAuth"
 import UsersList from "./features/users/UsersList"
+import Unauthorized from "./components/Unauthorized"
 
 
 
@@ -17,10 +18,14 @@ function App() {
         {/* public routes */}
         <Route index element={<Public />} />
         <Route path="login" element={<Login />} />
+        <Route path="unauthorized" element={<Unauthorized />} />
 
         {/* protected routes */}
-        <Route element={<RequireAuth />}>
+        <Route element={<RequireAuth allowedRoles={['admin', 'user']} />}>
           <Route path="welcome" element={<Welcome />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={['admin']} />}>
           <Route path="users" element={<UsersList />} />
         </Route>
 
--- src/components/RequireAuth.jsx
+++ src/components/RequireAuth.jsx
@@ -1,15 +1,33 @@
+/* eslint-disable no-unused-vars */
 import { useLocation, Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectCurrentToken } from "../features/auth/authSlice";
+import { jwtDecode } from 'jwt-decode'
 
-const RequireAuth = () => {
+// eslint-disable-next-line react/prop-types
+const RequireAuth = ({ allowedRoles }) => {
     const token = useSelector(selectCurrentToken);
     const location = useLocation();
 
+    const decoded = token ? jwtDecode(token) : null
+    console.log('decoded dari RequireAuth: ', decoded);
+    
+
+    const role = decoded ? decoded.userInfo.role : []
+    const bidang = decoded ? decoded.userInfo.bidang : []
+
+    
+    // eslint-disable-next-line react/prop-types
+    const result = allowedRoles.some(r => role?.includes(r))
+    console.log('result dari RequireAuth: ', result);
+    
+
     return (
-        token
+        result
             ? <Outlet />
-            : <Navigate to="/login" state={{ from: location }} replace />
+            : token
+                ? <Navigate to="/unauthorized" state={{ from: location }} replace />
+                : <Navigate to="/login" state={{ from: location }} replace />
     )
 }
 
